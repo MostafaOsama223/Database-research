c603b205998462a1b01964ca5d0908507bad93cb
Moustafa Tarek <62303206+moustafaTarek@users.noreply.github.com>
2020-06-10 16:52:14 +0200

    Add files via upload

---
+++ routes/delete_customer.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var conn =require('../dbConn.js');
+
+
+router.post('/customer',function(req,res){
+  var body=req.body;
+  var baseQuery= ' DELETE FROM `customer` WHERE `customer id`='+ body.customerID+ ';' ;
+
+  if(body.customerID != undefined)
+  console.log(baseQuery);
+  conn.query(baseQuery,function(err,result,fields){
+    if(err) res.send(err);
+    else res.send(result);
+
+  });
+
+});
+
+module.exports =router;
