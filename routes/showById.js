563a58db4272d3ce4db6e87ba26d610c94d1683d
MostafaOsama223 <mostafaosama03@hotmail.com>
2020-06-10 05:25:41 +0200

    Added endpoints transaction/deposit, transaction/draw and showById/..

--- routes/showById.js
+++ routes/showById.js
@@ -4,7 +4,18 @@ var conn = require('../dbConn');
 
 router.get('/customer/:id', function(req, res){
     var query = 'SELECT * FROM `customer` WHERE `customer id` = ' + req.params.id;
-    console.log(id);
+    conn.query(query, function(err, results, fields){
+        if(err) res.send(err);
+        else    res.send(results[0]);
+    });
+});
+
+router.get('/employee/:id', function(req, res){
+    var query = 'SELECT * FROM `employee` WHERE `employee id` = ' + req.params.id;
+    conn.query(query, function(err, results, fields){
+        if(err) res.send(err);
+        else    res.send(results[0]);
+    });
 });
 
 module.exports = router;
