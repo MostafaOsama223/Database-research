d9e33331046fe77c39a36e3fee00078b4c5e3a08
Moustafa Tarek <62303206+moustafaTarek@users.noreply.github.com>
2020-06-10 17:27:43 +0200

    Update add.js

--- routes/add.js
+++ routes/add.js
@@ -19,4 +19,22 @@ router.post('/customer', function(req, res){
     });   
 });
 
+router.post('/addEmployee', function(req, res){
+var body = req.body;
+var query = ' INSERT INTO `employee` (`employee name`, `employee id`, `employee salery`,\
+                                          `email`, `branch id`) VALUES ("'
+                                    +body.Name+'","'
+                                    +body.ID+'","'
+                                    +body.salary+'","'
+                                    +body.email+'","'
+                                    +body.branchID
+                                    +'")' ;
+
+      console.log(query);
+      conn.query(query,function(err,result,fields){
+        if(err) res.send(err);
+        else res.send(body);
+    });
+});
+
 module.exports = router;
